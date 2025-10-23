66db51ab539ebcacfb39da54e883fbb98dd6d9be
saeta728 <saeta728@users.noreply.github.com>
2025-10-23 13:31:39 -0500

    Cambio en Builder
    Cambio

--- builder.js
+++ builder.js
@@ -1,7 +1,7 @@
 // builder.js — ListaBuilder v2.0
 const $ = id => document.getElementById(id);
 
-let totpSecret = "SECRET_LISTABUILDER"; // mismo secreto guardado en tu repo o en Local
+let totpSecret = Mu7/sYIMjJw8hCpapRRM7g==; // mismo secreto guardado en tu repo o en Local
 let localData = { bloqueos: [], excepciones: [], excepcionesHorario: [] };
 
 // --- Inicialización ---
